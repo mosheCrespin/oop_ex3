574f76c53f35e9fb0f40fc57a6e059995829ff9e
moshe crespin <moshec315@gmail.com>
2021-01-04 09:33:17 +0200

    part one with tests

---
+++ src/DiGraph.py
@@ -0,0 +1,97 @@
+from src.GraphInterface import GraphInterface
+
+
+class NodeData:
+    def __init__(self, node_id: int, pos: tuple = None):
+        self.node_id = node_id
+        self.pos = pos
+
+    def __repr__(self):
+        return repr((self.node_id, self.pos))
+
+
+class DiGraph(GraphInterface):
+
+    def __init__(self):
+        self.number_of_nodes = 0
+        self.number_of_edges = 0
+        self.amount_of_changes = 0
+        self.my_graph = {}
+        self.out_edges = {}
+        self.in_edges = {}
+
+    def v_size(self) -> int:
+        return self.number_of_nodes
+
+    def e_size(self) -> int:
+        return self.number_of_edges
+
+    def get_mc(self) -> int:
+        return self.amount_of_changes
+
+    def has_edge(self, id1: int, id2: int) -> bool:
+        return not self.out_edges.get(id1).get(id2) is None
+
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        if not self.has_node(id1) and not self.has_node(id2):
+            return False
+        if self.has_edge(id1, id2):
+            return False
+        self.out_edges[id1][id2] = weight
+        self.in_edges[id2][id1] = weight
+        self.amount_of_changes += 1
+        self.number_of_edges += 1
+        return True
+
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        if self.has_node(node_id):  # the node already exist
+            return False
+        node = NodeData(node_id, pos)
+        self.my_graph[node_id] = node
+        self.out_edges[node_id] = {}
+        self.in_edges[node_id] = {}
+        self.number_of_nodes += 1
+        self.amount_of_changes += 1
+        return True
+
+    def has_node(self, node_id: int) -> bool:
+        return not self.my_graph.get(node_id) is None
+
+    def remove_node(self, node_id: int) -> bool:
+        if not self.has_node(node_id): return False
+        counter=0
+        for i in self.in_edges[node_id]:
+            del self.out_edges[i][node_id]
+            counter+=1
+        for i in self.out_edges[node_id]:
+            del self.in_edges[i][node_id]
+            counter+=1
+        del self.my_graph[node_id]
+        del self.out_edges[node_id]
+        del self.in_edges[node_id]
+        self.amount_of_changes += 1
+        self.number_of_edges -= counter
+        self.number_of_nodes -= 1
+        return True
+
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        if not self.has_node(node_id1) and not self.has_node(node_id2):
+            return False
+        if not self.has_edge(node_id1, node_id2):
+            return False
+        del self.out_edges[node_id1][node_id2]
+        del self.in_edges[node_id2][node_id1]
+        self.number_of_edges -= 1
+        self.amount_of_changes += 1
+        return True
+
+    def get_all_v(self) -> dict:
+        return self.my_graph
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        if not self.has_node(id1): return None
+        return self.in_edges[id1]
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        if not self.has_node(id1): return None
+        return self.out_edges[id1]
---
+++ src/test_DiGraph.py
@@ -0,0 +1,136 @@
+import unittest
+from DiGraph import DiGraph
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_add_node(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        self.assertEqual(10, g0.v_size())
+        self.assertEqual(10, g0.get_mc())
+        g0.add_node(9)  # added existed node
+        self.assertEqual(10, g0.v_size())
+        self.assertEqual(10, g0.get_mc())
+
+    def test_add_edge(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        for i in range(0, 10):
+            g0.add_edge(i, 9 - i, 0.2)
+        self.assertEqual(10, g0.e_size())
+        self.assertEqual(20, g0.get_mc())
+        self.assertFalse(g0.add_edge(0, 9, 3))  # added exited edge
+        self.assertEqual(10, g0.e_size())
+        self.assertEqual(20, g0.get_mc())
+
+    def test_remove_edge(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        for i in range(0, 10):
+            g0.add_edge(i, 9 - i, 0.2)
+        g0.remove_edge(0, 9)
+        self.assertEqual(9, g0.e_size())
+        self.assertEqual(21, g0.get_mc())
+
+    def test_remove_node(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        g0.add_edge(4, 3, 1)
+        g0.add_edge(4, 2, 1)
+        g0.add_edge(2, 4, 1)
+        g0.add_edge(1, 2, 1)
+        g0.add_edge(5, 4, 1)
+        g0.add_edge(2, 5, 1)
+        g0.remove_node(4)
+        self.assertEqual(g0.get_all_v().get(4), None)
+        self.assertEqual(0, len(g0.all_out_edges_of_node(5)))
+        self.assertEqual(9, g0.v_size())
+        self.assertEqual(2, g0.e_size())
+
+    def test_v_size(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        self.assertEqual(10, g0.v_size())
+
+    def test_e_size(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        for i in range(0, 10):
+            g0.add_edge(i, 9 - i, 0.2)
+        self.assertEqual(10, g0.e_size())
+
+    def test_get_all_v(self):
+        g0 = DiGraph()
+        flag = True
+        for i in range(0, 10):
+            g0.add_node(i)
+        for i in range(0, 10):
+            if i not in g0.get_all_v().keys(): flag = False
+        self.assertTrue(flag)
+
+    def test_all_in_edges_of_node(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        g0.add_edge(4, 3, 1)
+        g0.add_edge(4, 2, 1)
+        g0.add_edge(2, 4, 1)
+        g0.add_edge(1, 2, 1)
+        g0.add_edge(5, 3, 1)
+        g0.add_edge(2, 5, 1)
+        self.assertEqual(2, len(g0.all_in_edges_of_node(3)))
+        g0.add_edge(1, 3, 1)
+        self.assertEqual(3, len(g0.all_in_edges_of_node(3)))
+        g0.add_edge(1, 3, 1)  # already existed
+        self.assertEqual(3, len(g0.all_in_edges_of_node(3)))
+        g0.remove_edge(1, 3)  # check if this dict updates after removing edge
+        self.assertEqual(2, len(g0.all_in_edges_of_node(3)))
+
+    def test_all_out_edges_of_node(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        g0.add_edge(4, 3, 1)
+        g0.add_edge(4, 2, 1)
+        g0.add_edge(2, 4, 1)
+        g0.add_edge(1, 2, 1)
+        g0.add_edge(5, 3, 1)
+        g0.add_edge(4, 5, 1)
+        self.assertEqual(3, len(g0.all_out_edges_of_node(4)))
+        g0.add_edge(4, 6, 1)
+        self.assertEqual(4, len(g0.all_out_edges_of_node(4)))
+        g0.add_edge(4, 6, 1)  # already existed
+        self.assertEqual(4, len(g0.all_out_edges_of_node(4)))
+        g0.remove_edge(4, 6)  # check if this dict updates after removing edge
+        self.assertEqual(3, len(g0.all_out_edges_of_node(4)))
+
+    def test_mc(self):
+        g0 = DiGraph()
+        for i in range(0, 10):
+            g0.add_node(i)
+        g0.add_edge(4, 3, 1)
+        g0.add_edge(4, 2, 1)
+        g0.add_edge(2, 4, 1)
+        g0.add_edge(1, 2, 1)
+        g0.add_edge(5, 3, 1)
+        g0.add_edge(4, 5, 1)
+        self.assertEqual(16, g0.get_mc())
+        g0.add_edge(4, 5, 1)  # already exist
+        self.assertEqual(16, g0.get_mc())
+        g0.add_node(3)  # already exist
+        self.assertEqual(16, g0.get_mc())
+        g0.remove_edge(2, 4)
+        self.assertEqual(17, g0.get_mc())
+        g0.remove_edge(2, 4)  # removing twice
+        self.assertEqual(17, g0.get_mc())
+        g0.remove_node(2)
+        self.assertEqual(18, g0.get_mc())
+        g0.remove_node(2)  # adding twice
+        self.assertEqual(18, g0.get_mc())
